786a8bfd69f10f6eff135240b14d7d6799e5f84a
SAYANui <sayans2005@gmail.com>
2026-03-14 23:01:07 +0530

    fixed small mistake

--- all pattern/src/star1.java
+++ all pattern/src/star1.java
@@ -1,5 +1,6 @@
 //print star triangle//
-public class star {
+
+public class star1 {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= i; j++) {
