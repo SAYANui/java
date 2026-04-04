f2f8168a1db279f9efe516575d23821e44a0117d
SAYANui <sayans2005@gmail.com>
2026-04-04 22:58:38 +0530

    dryrun 1

---
+++ PlayGround/src/ABCBank.java
@@ -0,0 +1,11 @@
+class ABCBank{
+    int phone=312345;
+    double getPhone(){
+        System.out.print("phone number received");
+        return phone;
+    }
+    public static void main(String[] args) {
+        ABCBank bank=new ABCBank();
+        System.out.print(bank.getPhone());
+    }
+}
