8b0ebe50ab56ed3f343fe72c244f519968a575c0
SAYANui <sayans2005@gmail.com>
2026-04-17 20:54:08 +0530

    (springboard  related (object)

--- PlayGround/Bank3.java
+++ PlayGround/Bank3.java
@@ -1,12 +1,12 @@
-class Bank {
+class Bank3 {
     private String bankName, area;
     private String phoneNumber;
-    Bank() {    // Default constructor
+    Bank3() {    // Default constructor
         bankName = "IBank";
         area = "Gandhi Nagar";
         phoneNumber = "9876543210";
     }
-    Bank(String bname, String barea, String phoneNo) {  // Parameterized constructor
+    Bank3(String bname, String barea, String phoneNo) {  // Parameterized constructor
         bankName = bname;
         area = barea;
         phoneNumber = phoneNo;
@@ -18,8 +18,8 @@ class Bank {
     }
 
     public static void main(String[] args){
-        Bank bank1 = new Bank();  //call default constructor
-        Bank bank2 = new Bank("IBank", "Jaydev Nagar", "8876543219"); //call Parameterized constructor
+        Bank3 bank1 = new Bank3();  //call default constructor
+        Bank3 bank2 = new Bank3("IBank", "Jaydev Nagar", "8876543219"); //call Parameterized constructor
 
         bank1.displayBankDetails();
         System.out.println("***********************");
