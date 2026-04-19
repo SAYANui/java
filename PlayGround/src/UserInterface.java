701047b2a60623f02044375dd5cf6f033727bf20
SAYANui <sayans2005@gmail.com>
2026-04-19 17:54:44 +0530

    springboard  related ( how enumerated data types work in Java )

---
+++ PlayGround/src/UserInterface.java
@@ -0,0 +1,20 @@
+
+enum DAY{
+    SUNDAY(1),MONDAY(2),TUESDAY(3),WEDNESDAY(4),THURSDAY(5),FRIDAY(6),SATURDAY(7);
+    private int value;
+    private DAY(int value){
+        this.value=value;
+    }
+    public int getValue(){
+        return this.value;
+    }
+}
+public class UserInterface{
+    public static void main(String[] args){
+        //printing all constants of an enum
+        for(DAY day:DAY.values())
+            System.out.println("Day:"+day.name()+" Value:"+day.getValue());
+    }
+}
+
+
