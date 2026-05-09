acf9f8f465c496ddde19e6e7dc074d08fbe21e7f
SAYANui <sayans2005@gmail.com>
2026-05-09 23:56:38 +0530

    small object created

---
+++ PlayGround/src/job.java
@@ -0,0 +1,19 @@
+class School{
+static int student=30;
+String name="nut bot";
+}
+
+
+
+public class job {
+
+public static void main (String[]args){
+
+
+    System.out.println(School.student);
+}
+
+
+
+
+}
