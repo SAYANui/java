9d68bfa4d54802a1b88be9da03aeae178cb6aabe
SAYANui <sayans2005@gmail.com>
2026-05-13 13:18:06 +0530

    calculater using while loop (updated)

--- PlayGround/src/Calculater.java
+++ PlayGround/src/Calculater.java
@@ -8,6 +8,8 @@ public static void main(String[]args){
     Scanner in=new Scanner(System.in);
 
     double ans=0;
+
+
     while(true) {     // taking all input in between while is true (means it will run forever util break hits //
         System.out.print("Enter first number :");
         double a = in.nextDouble();
@@ -29,22 +31,31 @@ else if(num=='/') {
     if (b!=0) {
         ans = a/b;
     }
+    else{
+        System.out.println("Invalid input");
+    }
            }
 else if(num=='%') {
     ans = a % b;
            }
-else if(num=='-') {
+else if( num =='-') {
     ans = a-b;
            }
-        System.out.println("\nthe ans is :"+ ans);
+        System.out.println("\nthe ans is :"+ (int)ans);
         System.out.println("*******************************************************************\n");
     }
+
+
+
+
+
+
        else if(num=='X'|| num=='x'){       // related to main if statement for break if the operator is X or x//
            break;
        }
        else  {
 
-           // if the operator is somthing else like L or h or etc its print invalid //
+           // if the operator is something else like L or h or etc. its print invalid //
            System.out.println("**********      invalid request     ************\n enter again ..............");
        }
     }
