079ab3c09e4ca034a550078727fdab1b4d1fe4fe
SAYANui <sayans2005@gmail.com>
2026-05-14 20:44:56 +0530

    small function

---
+++ PlayGround/src/function.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class function {
+
+    static void sum(){
+        Scanner input =new Scanner(System.in);
+        System.out.print("enter first number:");
+        int a=input.nextInt();
+        System.out.print("enter the second number:");
+        int b=input.nextInt();
+        int sum=a+b;
+        System.out.println("\n the sum is:"+sum);
+    }
+
+
+    public static void main (String[]args){
+
+sum();
+sum();
+
+
+
+
+    }
+
+}
