65e709615baea177b628b6308706badd9144d24a
SAYANui <sayans2005@gmail.com>
2026-05-15 21:26:34 +0530

    proper function  formate

--- PlayGround/src/function.java
+++ PlayGround/src/function.java
@@ -2,25 +2,21 @@ import java.util.Scanner;
 
 public class function {
 
-    static void sum(){
+
+
+    public static void main (String[]args){
+
         Scanner input =new Scanner(System.in);
         System.out.print("enter first number:");
         int a=input.nextInt();
         System.out.print("enter the second number:");
         int b=input.nextInt();
+        sum(a,b);
+
+    }
+    static void sum(int a,int b){
+
         int sum=a+b;
         System.out.println("\n the sum is:"+sum);
     }
-
-
-    public static void main (String[]args){
-
-sum();
-sum();
-
-
-
-
-    }
-
 }
