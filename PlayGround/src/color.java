11c4d10539fec3be00db1e51befa0631f0a2fdf6
SAYANui <sayans2005@gmail.com>
2026-05-16 13:27:28 +0530

    call by value example  for object and  the block format

--- PlayGround/src/color.java
+++ PlayGround/src/color.java
@@ -11,7 +11,25 @@ public class color {
 
         replace(myPaper);
         System.out.println(myPaper.color); // Prints: Red (Not Blue!)
+
+
+        int blue= 20;
+
+
+        {
+            blue =30; // int blue =30 is wrong (not again initialized ) //
+            int red =20;
+        }
+        int red=40; // true because red don't know the content inside BLOCK //
+        System.out.println("the actual red:"+ red); // red is 40  not 20
+
+
     }
+
+
+
+
+
 
     static void paint(Paper p) {
         p.color = "Red";
