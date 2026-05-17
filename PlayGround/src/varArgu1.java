871ccdf47c270dbf7641f95c6f7f4408956233c7
SAYANui <sayans2005@gmail.com>
2026-05-17 23:41:31 +0530

    2nd  vararg example (2 more data type added

--- PlayGround/src/varArgu1.java
+++ PlayGround/src/varArgu1.java
@@ -3,15 +3,15 @@ public class varArgu1 {
     static void main(String[]args) {
 
 
-               System.out.println(sum(4,4));
-                System.out.println("Sum: " + sum(1, 2, 3, 4, 5)); // Outputs 15
+        sum(4,4);
+        sum(1, 2, 3, 4, 5); // Outputs 15
         }
 
 
 
 
         // Method using a traditional for loop instead of a for-each loop
-        public static int sum(int... numbers) {
+        public static void sum(int a,float b,int... numbers) {
             int total = 0;
 
             // i starts at 0 (the first index) and goes up to the array's length minus 1
@@ -19,8 +19,10 @@ public class varArgu1 {
                 int n = numbers[i]; // Fetching the number at index i
                 total += n;        // Adding it to the total
             }
+           int add= (a+(int)b);
+            System.out.println(a+"+"+b+"="+add);
+            System.out.println("sum:"+total+"\n");
 
-            return total;
         }
 
 
