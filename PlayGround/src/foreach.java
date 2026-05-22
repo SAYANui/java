dd38b44681a731fb82e0ebbdfe092cbbc7ed138e
SAYANui <sayans2005@gmail.com>
2026-05-22 22:27:35 +0530

    small for each

---
+++ PlayGround/src/foreach.java
@@ -0,0 +1,17 @@
+public class foreach {
+
+    public static void main(String[] args) {
+
+        int[] arr={4,5,6,88,9996,44,};
+        for(int ar:arr){
+            System.out.print(ar+",");
+        }
+        System.out.println();
+        for (int i=0;i<arr.length;i++){
+
+           System.out.print(arr[i]+",");
+        }
+    }
+
+
+}
