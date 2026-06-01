6a432616f1336aab1d29c24ee1cec7b49f71c089
SAYANui <sayans2005@gmail.com>
2026-06-01 22:22:25 +0530

    updated

--- PlayGround/src/playGround.java
+++ PlayGround/src/playGround.java
@@ -1,9 +1,19 @@
+import java.util.Arrays;
 
 public class playGround {
 
 public static void main(String[]args){
-
+int[] a= {5,66,77,8};
+System.out.println(Arrays.toString(a));
+change(a);
+System.out.print(Arrays.toString(a));
 
         }
+
+        static void change(int[]arr){
+    arr[3]=55;
+        }
+
+
     }
 
