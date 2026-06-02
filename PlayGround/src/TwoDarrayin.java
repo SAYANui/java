7a9563dbeba6f930671efcdc2686b3a3160095a4
SAYANui <sayans2005@gmail.com>
2026-06-03 00:53:51 +0530

    taking inputs of 2d array and printing it in terminal

---
+++ PlayGround/src/TwoDarrayin.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class TwoDarrayin {
+
+public static void main(String[]args){
+    Scanner input =new Scanner(System.in);
+
+    String[][] arr= new String[2][4];
+    for (int a=0;a<arr.length;a++){
+     for (int b=0;b<arr[a].length;b++){
+       arr[a][b]=input.next();
+     }
+    }
+    System.out.println(Arrays.deepToString(arr));
+
+    input.close();
+
+}
+}
