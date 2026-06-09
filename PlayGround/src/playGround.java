f428c969fa79f1600658ae3f266a88a43cbfb68d
SAYANui <sayans2005@gmail.com>
2026-06-09 22:27:31 +0530

    practice 3d array

--- PlayGround/src/playGround.java
+++ PlayGround/src/playGround.java
@@ -2,17 +2,9 @@ import java.util.Arrays;
 
 public class playGround {
 
-public static void main(String[]args){
-int[] a= {5,66,77,8};
-System.out.println(Arrays.toString(a));
-change(a);
-System.out.print(Arrays.toString(a));
-
-        }
-        static void change(int[]arr){
-    arr[3]=55;
-        }
+public static void main(String[]args) {
 
 
+}
     }
 
